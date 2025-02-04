2b1a597847fd5e95806e271e093f8b692b98dc36
Rolf K <abstractstar@gmail.com>
2025-02-04 20:34:15 +0100

    Parse xml and print the data we have deserialized so far

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+mod setlists;
+
+use setlists::Setlists;
+
+fn main() -> std::io::Result<()> {
+    let file = std::fs::read_to_string("master.xml")?;
+
+    // let master: Setlists = serde_xml_rust::from_str(&file).unwrap();
+    let master = Setlists::from_xml(&file).unwrap();
+
+    for setlist in master.data {
+        println!(
+            "Artist: {} (mbid={})",
+            setlist.artist.name,
+            setlist.artist.mbid.unwrap_or("no mbid".to_string())
+        );
+        println!("Event Date: {}", setlist.event_date);
+        println!("Status: {}", setlist.status);
+        if let Some(source) = setlist.source {
+            println!("Source: {}", source);
+        }
+        println!("Venue: {}", setlist.venue.name);
+        println!("City: {}", setlist.venue.city.name);
+        println!("Country: {}", setlist.venue.city.country.name);
+        println!("--------------------");
+    }
+
+    Ok(())
 }
