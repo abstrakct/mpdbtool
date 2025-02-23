c023b48938c95d80fd84e8e0f2e985b72e644c84
Rolf K <abstractstar@gmail.com>
2025-02-23 11:55:23 +0100

    Add Slug trait
    Function comment says it all ;)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,25 @@ use std::collections::HashSet;
 
 mod tests;
 
+trait Slug {
+    fn slug(&self) -> String;
+}
+
+impl Slug for String {
+    /// Converts the string to lowercase, replaces all non-ASCII characters with
+    /// hyphens, and trims leading and trailing whitespace. Returns the modified
+    /// string.
+    fn slug(&self) -> String {
+        // convert to lowercase
+        // replace all non-ascii characters with a hyphen
+        // trim leading and trailing whitespace
+        self.to_lowercase()
+            .replace(|c: char| !c.is_ascii(), "-")
+            .trim_ascii()
+            .to_string()
+    }
+}
+
 #[derive(Deserialize, Debug)]
 struct Country {
     name: String,
