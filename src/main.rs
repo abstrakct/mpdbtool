557a3f4dafdb62ae8e84d747802353ae00e423db
Rolf K <abstractstar@gmail.com>
2025-02-23 12:05:38 +0100

    slug: also replace spaces with hyphens

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,16 @@ trait Slug {
 
 impl Slug for String {
     /// Converts the string to lowercase, replaces all non-ASCII characters with
-    /// hyphens, and trims leading and trailing whitespace. Returns the modified
-    /// string.
+    /// hyphens, replaces all spaces with hyphens, and trims leading and trailing
+    /// whitespace. Returns the modified string.
     fn slug(&self) -> String {
         // convert to lowercase
         // replace all non-ascii characters with a hyphen
+        // replace all spaces with a hyphen
         // trim leading and trailing whitespace
         self.to_lowercase()
             .replace(|c: char| !c.is_ascii(), "-")
+            .replace(" ", "-")
             .trim_ascii()
             .to_string()
     }
