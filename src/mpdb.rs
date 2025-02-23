2641a26ede2800b4b7019b79c868339da6f56668
Rolf K <abstractstar@gmail.com>
2025-02-23 12:19:40 +0100

    Move mpdb to its own module, and refactor a bit
    - Moves the various helper functions inside Mpdb
    - Adds base_url to Mpdb and use it instead of the const.
    - base_url must be initialized in new()

---
+++ src/mpdb.rs
@@ -0,0 +1,241 @@
+use mpdblib::*;
+use serde::Deserialize;
+use std::collections::HashSet;
+
+pub use crate::slug::*;
+
+#[derive(Deserialize, Debug)]
+pub struct Country {
+    id: i32,
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct City {
+    id: i32,
+    name: String,
+    country_id: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Venue {
+    id: i32,
+    name: String,
+    city_id: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Mpdb {
+    base_url: String,
+    pub master: Setlists,
+    pub countries: Vec<Country>,
+    pub cities: Vec<City>,
+    pub venues: Vec<Venue>,
+}
+
+impl Mpdb {
+    pub fn new(base_url: String) -> Mpdb {
+        Mpdb {
+            base_url,
+            master: Setlists::new(),
+            countries: vec![],
+            cities: vec![],
+            venues: vec![],
+        }
+    }
+
+    fn extract_all_country_names(&self) -> HashSet<String> {
+        self.master
+            .data
+            .iter()
+            .map(|s| s.venue.city.country.name.clone())
+            .collect()
+    }
+
+    fn get_all_cities(&self) -> HashSet<(String, String)> {
+        self.master
+            .data
+            .iter()
+            .map(|s| (s.venue.city.name.clone(), s.venue.city.country.name.clone()))
+            .collect()
+    }
+
+    fn get_all_venues(&self) -> HashSet<(String, String, String)> {
+        self.master
+            .data
+            .iter()
+            .map(|s| {
+                (
+                    s.venue.name.clone(),
+                    s.venue.city.name.clone(),
+                    s.venue.city.country.name.clone(),
+                )
+            })
+            .collect()
+    }
+
+    #[allow(dead_code)]
+    fn setlists_to_db(master: Setlists) -> reqwest::Result<()> {
+        let setlist = master.data[3].clone();
+        let x = serde_json::to_string(&setlist).unwrap();
+        println!("{}", x);
+
+        //let countries = get_all_countries(&master);
+        //println!("{:?}", countries);
+        //let cities = get_all_cities(&master);
+        //println!("{:?}", cities);
+
+        Ok(())
+    }
+
+    fn get_country_id(&self, country_name: &str) -> Option<i32> {
+        self.countries
+            .iter()
+            .find(|c| c.name == country_name)
+            .map(|c| c.id)
+    }
+
+    fn get_city_id(&self, city_name: &str, country_name: &str) -> Option<i32> {
+        self.cities
+            .iter()
+            .find(|c| {
+                c.name == city_name && c.country_id == self.get_country_id(country_name).unwrap()
+            })
+            .map(|c| c.id)
+    }
+
+    pub async fn add_all_countries(&self) -> reqwest::Result<Vec<Country>> {
+        let countries = self.extract_all_country_names();
+        let client = reqwest::Client::new();
+        let url = format!("{}/api/countries", self.base_url);
+
+        let existing_countries = client.get(&url).send().await?;
+        let existing_countries: Vec<Country> = existing_countries.json().await?;
+        let existing_countries: HashSet<String> =
+            existing_countries.iter().map(|c| c.name.clone()).collect();
+
+        // println!("Existing countries: {existing_countries:?}");
+
+        for country in countries {
+            println!("Adding country: {country}");
+
+            // Check if country already exists
+            if existing_countries.contains(&country) {
+                println!("Country '{country}' already exists - skipping.");
+                continue;
+            }
+
+            // Country doesn't exist, so add it
+            let data = serde_json::json!({"name": country});
+            let res = client.post(&url).json(&data).send().await?;
+            if res.status().is_success() {
+                println!("Added country:  {country}");
+            } else {
+                println!("Error adding country: {country}");
+            }
+        }
+
+        let existing_countries = client.get(&url).send().await?;
+        let existing_countries: Vec<Country> = existing_countries.json().await?;
+        Ok(existing_countries)
+    }
+
+    pub async fn add_all_cities(&self) -> reqwest::Result<Vec<City>> {
+        let cities = self.get_all_cities();
+        let client = reqwest::Client::new();
+        let url = format!("{}/api/cities", self.base_url);
+
+        let existing_cities = client.get(&url).send().await?;
+        let existing_cities: Vec<City> = existing_cities.json().await?;
+        let existing_cities: HashSet<(String, i32)> = existing_cities
+            .iter()
+            .map(|c| (c.name.clone(), c.country_id))
+            .collect();
+
+        println!("Existing cities: {existing_cities:?}");
+
+        for city in cities {
+            println!("Adding city: {} in country: {}", city.0, city.1);
+
+            if let Some(country_id) = self.get_country_id(&city.1) {
+                // Check if city already exists
+                if existing_cities.contains(&(city.0.clone(), country_id)) {
+                    // TODO: send update request instead of skipping?
+                    println!(
+                        "City '{}' in country '{}' already exists - skipping.",
+                        city.0, city.1
+                    );
+                    continue;
+                }
+
+                // City doesn't exist, so add it
+                let data = serde_json::json!({"name": city.0, "country_id": country_id});
+                let res = client.post(&url).json(&data).send().await?;
+                if res.status().is_success() {
+                    println!("Added city: {} in country: {}", city.0, city.1);
+                } else {
+                    println!("Error adding city: {} in country: {}", city.0, city.1);
+                }
+            }
+        }
+
+        let existing_cities = client.get(&url).send().await?;
+        let existing_cities: Vec<City> = existing_cities.json().await?;
+        Ok(existing_cities)
+    }
+
+    pub async fn add_all_venues(&self) -> reqwest::Result<Vec<Venue>> {
+        let venues = self.get_all_venues();
+        let client = reqwest::Client::new();
+        let url = format!("{}/api/venues", self.base_url);
+
+        let existing_venues = client.get(&url).send().await?;
+        let existing_venues: Vec<Venue> = existing_venues.json().await?;
+        let existing_venues: HashSet<(String, i32)> = existing_venues
+            .iter()
+            .map(|c| (c.name.clone(), c.city_id))
+            .collect();
+
+        println!("Existing venues: {existing_venues:?}");
+
+        for venue in venues {
+            println!(
+                "Adding venue: {} in city: {} in country: {}",
+                venue.0, venue.1, venue.2
+            );
+
+            if let Some(city_id) = self.get_city_id(&venue.1, &venue.2) {
+                // Check if venue already exists
+                if existing_venues.contains(&(venue.0.clone(), city_id)) {
+                    println!(
+                        "venue '{}' in city '{}' already exists - skipping.",
+                        venue.0, venue.1
+                    );
+                    continue;
+                }
+
+                // venue doesn't exist, so add it
+                let unique_name = format!("{}-{}", venue.0.slug(), venue.1.slug());
+                let data = serde_json::json!({
+                    "name": venue.0,
+                    "city_id": city_id,
+                    "unique_name": unique_name
+                });
+                let res = client.post(&url).json(&data).send().await?;
+
+                if res.status().is_success() {
+                    println!("Added venue: {} in city: {}", venue.0, venue.1);
+                } else {
+                    println!(
+                        "Error adding venue: {} in city: {} - city id {city_id}",
+                        venue.0, venue.1
+                    );
+                }
+            }
+        }
+
+        let existing_venues = client.get(&url).send().await?;
+        let existing_venues: Vec<Venue> = existing_venues.json().await?;
+        Ok(existing_venues)
+    }
+}
