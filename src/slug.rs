63712fd30a5792e311441cb0ff9635774580eaa3
Rolf K <abstractstar@gmail.com>
2025-02-28 21:36:58 +0100

    Improve Slug trait again

--- src/slug.rs
+++ src/slug.rs
@@ -5,19 +5,22 @@ pub trait Slug {
 impl Slug for String {
     /// Returns a slug for the string.
     ///
-    /// The slug is created by
+    /// The slug is created by:
     ///
-    /// 1. Replacing some special characters with a-z
-    /// 2. Lowercasing the string
-    /// 3. Replacing all non-alphanumeric characters with a hyphen
-    /// 4. Replacing all spaces with a hyphen
-    /// 5. Trimming any leading or trailing hyphens
+    /// 1. Replacing 'Ü' with 'u'
+    /// 2. Converting the string to lowercase
+    /// 3. Replacing all non-alphanumeric characters with hyphens
+    /// 4. Replacing multiple consecutive hyphens with a single hyphen
+    /// 5. Trimming any trailing hyphens
     fn slug(&self) -> String {
         self.replace("Ü", "u")
             .to_lowercase()
             .replace(|c: char| !c.is_alphanumeric(), "-")
             .replace(" ", "-")
             .trim_ascii()
+            .replace("---", "-")
+            .replace("--", "-")
+            .trim_end_matches('-')
             .to_string()
     }
 }
